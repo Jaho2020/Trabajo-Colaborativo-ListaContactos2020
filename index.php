679cffedbfe8b8aafbda3dac40007cdeec77a6f6
Jaho2020 <jhcastro2020@gmail.com>
2020-06-07 19:44:55 -0500

    Se crean contendores en el index

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
-<?php include 'inc/vista/header.php': ?>
+
+
+<?php include 'inc/vista/header.php'; ?>
 
 <div class="contenedor-barra">
-	<h1>Agenda de Contactos</h1>
+	<h1>Agenda de Contactos - Integracion Continua</h1>
 </div>
 
+<div class="bg-amarillo contenedor sombra">
+	<form id="contacto" action="#">
+		<legend>Añada un campo 
+			<span>Todos los campos son obligatorios	</span> 
+		</legend>
 
-<?php include 'inc/vista/footer.php': ?>
+		<div class="campos">
+
+			<div class="campo">
+				<label for="nombre">Nombre</label>
+			    <input type="text" placeholder="Nombre contacto" id="nombre">
+			</div>
+			<div class="campo">
+				<label for="empresa">Empresa</label>
+			    <input type="text" placeholder="Nombre empresa" id="empresa">
+			</div>
+			<div class="campo">
+				<label for="nombre">Telefono</label>
+			    <input type="tel" placeholder="Nombre de contacto" id="nombre">
+			</div>
+			<div class="campo enviar">
+				<input type="submit" value="AÑADIR">
+			</div>
+			
+		</div>
+
+
+	</form>
+</div>
+
+<?php include 'inc/vista/footer.php'; ?>
 
