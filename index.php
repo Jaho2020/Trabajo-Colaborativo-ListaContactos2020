df9daf9f7c71b30a19edb211c9435c08cb183276
Jaho2020 <jhcastro2020@gmail.com>
2020-06-07 23:09:46 -0500

    Detalles finales del primer formulario
    Estilo al primer formulario

--- index.php
+++ index.php
@@ -15,22 +15,22 @@
 		<div class="campos">
 
 			<div class="campo">
-				<label for="nombre">Nombre</label>
-			    <input type="text" placeholder="Nombre contacto" id="nombre">
+				<label for="nombre">Nombre:</label>
+			    <input type="text" placeholder="Nombre Contacto" id="nombre">
 			</div>
 			<div class="campo">
-				<label for="empresa">Empresa</label>
-			    <input type="text" placeholder="Nombre empresa" id="empresa">
+				<label for="empresa">Empresa:</label>
+			    <input type="text" placeholder="Nombre Empresa" id="empresa">
 			</div>
 			<div class="campo">
-				<label for="nombre">Telefono</label>
-			    <input type="tel" placeholder="Nombre de contacto" id="nombre">
+				<label for="nombre">Telefono:</label>
+			    <input type="tel" placeholder="Telefono De Contacto" id="nombre">
 			</div>
 			
 			
 		</div>
 
-		<div class="campo enviar">
+		    <div class="campo enviar">
 				<input type="submit" value="AÑADIR">
 			</div>
 
