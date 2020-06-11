a9e1bbfcdc7a74c1d5e03b044403eb78affc1bde
Jaho2020 <jhcastro2020@gmail.com>
2020-06-11 18:10:44 -0500

    Update bd.php

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -2,7 +2,7 @@
 
 // credenciales de la base de datos
 define('DB_USUARIO', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'jaho2020');
 define('DB_HOST', 'localhost');
 define('DB_NOMBRE', 'agendaphp');
 
