cf53cc5b3401d67b6cedb2acab2bb61ee7ae6267
Jaho2020 <jhcastro2020@gmail.com>
2020-06-11 13:40:42 -0500

    Trabajando con el JavaScrip
    se desarrolla notificacion al añadir contacto cuando los campos no estan completos generando error

--- js/app.js
+++ js/app.js
@@ -16,9 +16,33 @@ function leerFormulario(e){
 	      telefono = document.querySelector('#telefono').value;
 
 	if (nombre === '' || empresa === '' || telefono === '') {
-		console.log('Al menos un campo esta vacio');
+		//2 parametros: texto y clase
+		mostrarNotificacion('Todos los Campos son Obligatorios', 'error');
 	}else {
 		console.log('Tiene datos');
 	}      
 	
+}
+
+// Notificacion en pantalla  campo vacio
+function mostrarNotificacion(mensaje, clase){
+	const notificacion = document.createElement('div');
+	notificacion.classList.add(clase,'notificacion', 'sombra');
+	notificacion.textContent = mensaje;
+
+	//formulario
+	formularioContactos.insertBefore(notificacion, document.querySelector('form legend'));
+
+	//Ocultar y mostrar la notificacion en pantalla
+	setTimeout(() => {
+		notificacion.classList.add('visible');
+
+		setTimeout(() => {
+			notificacion.classList.remove('visible');
+			setTimeout(() =>{
+				notificacion.remove();
+			},500);
+			     
+		},3000);
+	},100);
 }
